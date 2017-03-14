0e85ba04987c5120620ff3924f3c50b3e712ac49
Jonathan Chou <jonchou@ucdavis.edu>
2017-03-14 16:57:47 -0700

    (feat) Created three input fields for origin, destination, and time of day.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'client/public');
-var APP_DIR = path.resolve(__dirname, 'client/app');
+var APP_DIR = path.resolve(__dirname, 'client/app/components');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
