8256f4c8cfe4863e36660baa3bd252de8eacde23
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-14 15:00:42 -0700

    Configure babel and initial react structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var BUILD_DIR = path.resolve(__dirname, 'client/public');
+var APP_DIR = path.resolve(__dirname, 'client/app');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
   }
 };
 
