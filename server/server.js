3cfd7c60b339c2c1b17d74f7a0e8509452004ca3
Jonathan Chou <jonchou@ucdavis.edu>
2017-03-14 21:44:45 -0700

    (bug) Uses the correct PORT variable.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var apiCall = require('./Models/OpenDataCaller');
 
 
