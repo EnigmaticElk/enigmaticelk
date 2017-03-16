11ef69bb57da8c877e643dfd71be7f3227b24d85
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-16 16:17:04 -0700

    Add empty mocha tests

---
+++ test/ServerSpec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+
+describe('server', function() {
+  it('should respond to GET requests', function() {
+    //some test code
+  })
+  it('should do some other stuff', function() {
+    //some other test code
+  })
+})
