c31ad721f9c3b690830df508a41d69c85c829627
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-16 18:14:36 -0700

    add basic server tests

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,10 +1,40 @@
-var chai = require('chai');
+var expect = require('chai').expect;
+var request = require('supertest');
+var express = require('express');
+var app = require('../server/server.js');
 
-describe('server', function() {
-  it('should respond to GET requests', function() {
-    //some test code
+describe('Server', function() {
+
+  describe('GET /', function() {
+    it('should respond to GET requests', function(done) {
+      request(app)
+        .get('/')
+        .expect(200, done);
+    })
   })
-  it('should do some other stuff', function() {
-    //some other test code
+
+  describe('POST /search', function() {
+    it('should respond to POST requests', function(done) {
+      request(app)
+        .post('/search')
+        .send({
+          address: '123 main street'
+        })
+        .expect(200)
+        .end(done);
+    })
+    it('should send coordinates when sent an address', function(done) {
+      request(app)
+        .post('/search')
+        .send({
+          address: '123 main street'
+        })
+        .expect(function(res) {
+          expect(res.body).to.be.an('object')
+          expect(res.body.lat).to.be.a('number')
+        })
+        .end(done);
+    })
   })
+
 })
