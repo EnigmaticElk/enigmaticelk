207ea068a833a106b4e76e74417199c6a6f2a0e9
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-16 22:35:02 -0700

    Revert "Feat/add heatmap points to map"

--- client/app/components/gmap.jsx
+++ client/app/components/gmap.jsx
@@ -8,7 +8,6 @@ class Gmap extends React.Component {
     this.state = {
 
     }
-    this.overlayHeatmap = this.overlayHeatmap.bind(this);
   }
 
   mapCenterLatLng() {
@@ -38,27 +37,6 @@ class Gmap extends React.Component {
       });
       map.fitBounds(bounds);
     }
-
-    if (this.props.heatmapData.length > 0) {
-      this.overlayHeatmap();
-    }
-  }
-
-  overlayHeatmap() {
-
-    var heatmapPoints = this.props.heatmapData.map(function(crime) {
-      return new google.maps.LatLng(crime.location.coordinates[1] - 0, crime.location.coordinates[0] - 0);
-    });
-
-    // Google heatmap layer has upper limites so we can't render all at once for right now
-    var heatmapSlice = heatmapPoints.slice(0, 58000);
-
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapSlice,
-      radius: 30,
-      map: this.state.map
-    });
-
   }
 
   render() {
