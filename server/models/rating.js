23f1297957d692c24b137e9c9f27c330be656a0d
Benicio Peinado <BenicioPeinado@gmail.com>
2017-03-21 15:53:30 -0700

    Finish logic to count crimes and assign labels

--- server/models/rating.js
+++ server/models/rating.js
@@ -1,16 +1,10 @@
 var db = require('../../database/index').db;
 var Rating = require('../../database/models/Rating');
 
-var clearCollection = function(callback) {
-  Rating.remove({}, function(err) {
-    callback();
-  });
-};
-
-// if street isn't found an empty array is returned, not an error, so to make the callback work an empty array is treated as if it were an error
 var findRatingEntry = function(street, callback) {
   Rating.find({street, street}, function(err, results) {
     if (results.length < 1) {
+    // if street isn't found an empty array is returned, not an error, so to make the callback work an empty array is treated as if it were an error
       err = results;
       callback(err, null);
     } else {
@@ -39,6 +33,28 @@ var createRatingEntry = function(street, callback) {
   });
 };
 
+var findAllRatings = (callback) => {
+  Rating.find({}, function(err, results) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var assignOneRating = function(street, rating, callback) {
+  Rating.findOneAndUpdate({street: street}, {$set:{rating: rating}}, {new: true}, function(err, results) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+}
+
 module.exports.findRatingEntry = findRatingEntry;
 module.exports.updateRatingEntry = updateRatingEntry;
 module.exports.createRatingEntry = createRatingEntry;
+module.exports.findAllRatings = findAllRatings;
+module.exports.assignOneRating = assignOneRating;
