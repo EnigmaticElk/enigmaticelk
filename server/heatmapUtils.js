91eaf0767ba26340571c2bbc9552483b6c0f0e16
dshelters <dshelters@gmail.com>
2017-03-22 02:01:57 -0700

    (feat) Find number of crimes committed on a given line
    Added a lot of different code while experimenting.
    Unable to find intersections by using crime data as points. Made crimes into squares as well as points.
    Finds the number of crimes, as boxes, that intersect a given line.

--- server/heatmapUtils.js
+++ server/heatmapUtils.js
@@ -1,4 +1,5 @@
 var db = require('./models/crime');
+var dbBox = require('./models/boxCrime');
 
 var crimeLocs = function (callback) {
   db.findLocations(function(results) {
@@ -16,3 +17,36 @@ var nearbyCrimes = function (callback) {
 };
 
 module.exports.nearbyCrimes = nearbyCrimes;
+
+
+var findCrimesByLine = function (directions, callback) {
+  console.log('crimes by line, not boxes');
+  db.findCrimesByLine(directions, function(crimesByStreet) {
+    callback(crimesByStreet.length);
+  });
+};
+
+module.exports.findCrimesByLine = findCrimesByLine;
+
+var findBoxCrimesByLine = function (directions, callback) {
+  dbBox.findBoxCrimesByLine(directions, function(crimesByStreet) {
+    var results = crimesByStreet.length;
+    callback(results);
+  });
+};
+
+module.exports.findBoxCrimesByLine = findBoxCrimesByLine;
+
+var findAllBoxes = function(callback) {
+  dbBox.findAllBoxes(function(results) {
+    callback(results);
+  })
+}
+module.exports.findAllBoxes = findAllBoxes;
+
+var findAll = function(callback) {
+  db.findAll(function(results) {
+    callback(results);
+  })
+}
+module.exports.findAll = findAll;
