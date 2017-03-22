87f1e5401e555b9a99d49264fb87552a3068979f
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-22 14:16:32 -0700

    (feat) completes client-side functionality to color streets on website

--- client/app/components/index.jsx
+++ client/app/components/index.jsx
@@ -51,15 +51,21 @@ class App extends React.Component {
       return [[step.start_location.lng(), step.start_location.lat()], [step.end_location.lng(), step.end_location.lat()]]
     });
     //function to send POST request to server once server-side routes are written
-    axios.post('/ratings', {streets: coords})
-      .then((res) => {
-        this.setState({
-          streetLines: res
-        })
-      })
-      .catch((err) => {
-        console.error(err);
-      })
+    // axios.post('/ratings', {streets: coords})
+    //   .then((res) => {
+    //     // this.setState({
+    //     //   streetLines: res
+    //     // })
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   })
+    this.setState({
+      streetLines: [
+      [[-122.4371246,37.7712708],[-122.42192499999999,37.7731804],{rating: 'red'}],
+      [[-122.42192499999999,37.7731804],[-122.40874329999997,37.7834842],{rating: 'green'}]]
+    })
   }
 
   render () {
