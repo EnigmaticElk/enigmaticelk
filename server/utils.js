89e5132661d14a2c4451e66537350fcb2d3af8ba
Benicio Peinado <BenicioPeinado@gmail.com>
2017-03-23 15:39:46 -0700

    Finish refactoring based on pull request comments

--- server/utils.js
+++ server/utils.js
@@ -9,7 +9,7 @@ var getCrimeLocs = function (callback) {
   });
 };
 
-var convertLatLngToStreet = function(lng, lat, callback) {
+var assignStreetFromLngLat = function(lng, lat, callback) {
   var url =`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`;
   request(url, function(err, res, body) {
     if (err) {
@@ -33,19 +33,19 @@ var convertLatLngToStreet = function(lng, lat, callback) {
 };
 
 var convertDirectionsToStreet = function(req, callback) {
-  var coordinatesWithAddresses = req.body.streets;
+  var coordsWithAddresses = req.body.streets;
   var counter = 0;
   for (var i = 0; i < req.body.streets.length; i++) {
     (function(i){
       setTimeout(function() {
-        convertLatLngToStreet(req.body.streets[i][0][0], req.body.streets[i][0][1], function(err, results) {
+        assignStreetFromLngLat(req.body.streets[i][0][0], req.body.streets[i][0][1], function(err, results) {
           if (err) {
             callback(err, null);
           } else {
-            coordinatesWithAddresses.push(results[0]);
+            coordsWithAddresses[i].push(results[0]);
             counter++;
             if (counter === req.body.streets.length) {
-              callback(null, coordinatesWithAddresses);
+              callback(null, coordsWithAddresses);
             }
           }
         });
@@ -55,5 +55,4 @@ var convertDirectionsToStreet = function(req, callback) {
 }
 
 module.exports.getCrimeLocs = getCrimeLocs;
-module.exports.convertLatLngToStreet = convertLatLngToStreet;
 module.exports.convertDirectionsToStreet = convertDirectionsToStreet;
