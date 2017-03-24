fb2d45928b0314ee45495597b11979084a6cd8d1
dshelters <dshelters@gmail.com>
2017-03-24 10:04:14 -0700

    (refact) Remove all references to the boxCrime table or it's box functions

--- server/server.js
+++ server/server.js
@@ -32,18 +32,6 @@ app.post('/ratings', function(req, res) {
 });
 
 
-app.get('/boxCrimesByStreet', function(req, res) {
-  
-  var test = [[[-122.410312, 37.782182], [-122.405677, 37.778527]]];
-  var test2 = [[[-122.419684, 37.782110], [-122.416702, 37.782458]]];
-  var test3 = [[[-122.405261, 37.773710], [-122.394992, 37.781952]]]
-  var test4 = test.concat(test2.concat(test3));
-  
-  utils.findBoxCrimesByLine(test4, function(crimesPerStreet) {
-    res.send(JSON.stringify(crimesPerStreet));
-  });
-});
-
 app.get('/crimesByStreet', function(req, res) {
   
   var test = [[[-122.410312, 37.782182], [-122.405677, 37.778527]]];
@@ -56,11 +44,6 @@ app.get('/crimesByStreet', function(req, res) {
   });
 });
 
-app.get('/boxCrimes', function(req, res) {
-  utils.findAllBoxes(function(boxCrimes) {
-    res.json(JSON.stringify(boxCrimes));
-  });
-});
 
 app.get('/allCrimes', function(req, res) {
   utils.findAllCrimes(function(crimes) {
