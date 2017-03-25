4aa109a6fa0943f01cf2231e3ce3a721ecb793a7
dshelters <dshelters@gmail.com>
2017-03-24 21:46:56 -0700

    (conflict) Fix some conflicts after rebasing.

--- server/workers/openDataCaller.js
+++ server/workers/openDataCaller.js
@@ -22,11 +22,6 @@ request(requestQuery, (err, res, body) => {
       .catch((err) => {
         console.error(err);
       });
-<<<<<<< HEAD
-=======
     });
-          console.log('length result from api call', results.length);
->>>>>>> (bug) Work towards updating geo queries with updated master
-  }
 });
 
