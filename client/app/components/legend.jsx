46015c4f1f21b054b61a05afab39155e4f2ada16
dshelters <dshelters@gmail.com>
2017-03-25 15:37:56 -0700

    (feat) Add legend with colors under the map.

--- client/app/components/legend.jsx
+++ client/app/components/legend.jsx
@@ -1,21 +1,65 @@
 import React from 'react';
 
 let Legend = (props) => {
-  let high = 'High Risk';
-  let mod = 'Moderate Risk';
-  let safe = 'Safe';
+  let high = {
+    desc:'High',
+    style: {
+      display: 'block',
+      width: '75px',
+      color: 'red',
+      paddingTop: '7px',
+      borderBottom: '5px solid red',
+    }
+  };
+
+  let mod = {
+    desc: 'Moderate',
+    style: {
+      display: 'block',
+      width: '75px',
+      color: 'yellow',
+      paddingTop: '7px',
+      borderBottom: '5px solid yellow',
+    }
+  };
+
+  let safe = {
+    desc: 'Safe',
+    style: {
+      display: 'block',
+      width: '75px',
+      color: 'green',
+      paddingTop: '7px',
+      borderBottom: '5px solid green',
+    }
+  };
+
+  // let hrStyle = {
+  //   display: 'block',
+  //   // align: '0',
+  //   // height: '1px',
+  //   // border: '0',
+  //   color: 'red',
+  //   // margin: '1em 0',
+  //   // padding: '0',
+  //   width: '75px',
+  //   // left: '0',
+  // };
+
   return (
-    <div>
-      <tr>
-        <td>{high}</td>
-      </tr>
-      <tr>
-        <td>{mod}</td>
-      </tr>
-      <tr>
-        <td>{safe}</td>
-      </tr>
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td>{high.desc}</td><td style={high.style}></td>
+        </tr>
+        <tr>
+          <td>{mod.desc}</td><td style={mod.style}></td>
+        </tr>
+        <tr>
+          <td>{safe.desc}</td><td style={safe.style}></td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
