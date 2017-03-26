7b6332e1be95326a9682a9459afa07e4828c1da6
dshelters <dshelters@gmail.com>
2017-03-26 03:41:36 -0700

    (feat) Make progress towards showing info on mouse over
    Currently broken and about to checkout previous commit.

--- client/app/components/Row.jsx
+++ client/app/components/Row.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
-let Row = (props) => {
-  let style = {
-    display: 'block',
-    width: '75px',
-    paddingTop: '7px',
-    borderBottom: `5px solid ${props.color}`,
-  };
+class Row extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    
+    let style = {
+      display: 'block',
+      width: '75px',
+      paddingTop: '7px',
+      borderBottom: `5px solid ${this.props.color}`,
+    };
 
-  return (
-    <tr>
-      <td>{props.desc}</td><td style={style}></td><td> >= {props.num} crimes on this stretch in last 30 days</td>
-    </tr>
-  )
+    this.showInfo = this.showInfo.bind(this);
+  }
+
+  showInfo() {
+    return `${this.props.num} crimes on this stretch in last 30 days`;
+  }
+
+  render() {
+    return (
+      <tr>
+      </tr>
+    )
+  }
 };
+        // <td>{this.props.desc}</td><td style={this.style}></td><td>{this.showInfo}</td>
 
 export default Row;
