84994666727d8054afdc861c882ca2a14af33048
Tyler Arbus <tyler.arbus@gmail.com>
2017-03-27 14:04:37 -0700

    (feat) updated text in guide

--- client/app/components/Guide.jsx
+++ client/app/components/Guide.jsx
@@ -17,13 +17,18 @@ class Guide extends React.Component {
     return (
       <div>
         <Dialog
-          title="How to Use"
+          title="How to use"
           modal={false}
           actions={actions}
           open={this.props.open}
           onRequestClose={this.props.toggle}
         >
         To get started, enter an origin and destination address.
+        The map will then display your requested route with an overlay of steet 'safeness' based on SF crime data.
+        <br />
+        <br />
+        Adjust your route using the alternative routes provided, or drag the directions line to choose a custom route.
+        Street safety ratings will automatically update.
         </Dialog>
       </div>
     );
