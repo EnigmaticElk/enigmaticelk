adf075e13634f834ea6b62eaae7f7b0afcb1a212
dshelters <dshelters@gmail.com>
2017-03-27 10:07:30 -0700

    (refact) Move ratingInfo file back out of models folder.

--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@ let dbCrime = require('./models/crime.js');
 let dbRating = require('./models/rating.js');
 let request = require('request');
 let GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || require('./googleMapsConfig.js');
-let calcRating = require('./models/ratingInfo').calcRating;
+let calcRating = require('./ratingInfo').calcRating;
 
 
 let getCrimeLocs = dbCrime.findLocations;
